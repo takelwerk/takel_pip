7088d356a4cf52b52e8f5be75cd764676db8e579
Stephan <stephan@takelwerk.net>
2023-07-23 14:16:44 +0200

    Update hg repos

--- molecule/default/system/test_takel_pip_system.py
+++ molecule/default/system/test_takel_pip_system.py
@@ -17,6 +17,8 @@ def get_pip_binry(host, testvars):
 def test_takel_pip_system_check_version(host, testvars, python3):
     expected_pip_packages = testvars['takel_pip_packages']
     for package in expected_pip_packages:
+        if 'skip_version_check' in package and package['skip_version_check']:
+            continue
 
         # do not check dependency meta packages
         if "[" in package['name']:
@@ -27,7 +29,14 @@ def test_takel_pip_system_check_version(host, testvars, python3):
         else:
             package_name = package["name"]
 
-        assert host.run_test(f"{python3} -c 'import {package_name}'"), \
+        # derive import name from package name
+        dirstr = 'import pkg_resources; ' \
+                 'print(pkg_resources.get_distribution(' \
+                 f"'{package_name}').egg_info)"
+        importstr = f"cat $(python3 -c \"{dirstr}\")/top_level.txt | tail -1"
+        package_import = host.check_output(importstr)
+
+        assert host.run_test(f"{python3} -c 'import {package_import}'"), \
             f"Failed to import {package['name']}"
 
         if 'version' not in package.keys() or \
@@ -35,8 +44,8 @@ def test_takel_pip_system_check_version(host, testvars, python3):
             continue
 
         installed_version = host.check_output(
-            f"{python3} -c 'import {package_name}; "
-            f"print({package_name}.__version__)'")
+            f"{python3} -c 'import {package_import}; "
+            f"print({package_import}.__version__)'")
         assert str(package['version']) in str(installed_version), \
             (f"Expected version for {package['name']} is "
              f"{package['version']}, but {str(installed_version)}"
